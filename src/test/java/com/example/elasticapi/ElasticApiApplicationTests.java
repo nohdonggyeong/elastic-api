71a78a18396278ecd25b718ad5c0be0e18041a7d
MartinNoh <ehdrud1129@naver.com>
2024-07-07 03:12:59 +0900

    first commit
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

---
+++ src/test/java/com/example/elasticapi/ElasticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
